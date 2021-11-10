ef06740d41777bc446d18d6f9c2e6abe18414fab
Carlos Hernandez Topete <chtopete@gmail.com>
2021-11-10 14:37:38 +0900

    restaurants controller added

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,11 @@ puts 'Cleaning database...'
 Restaurant.destroy_all
 
 puts 'Creating restaurants...'
-maki_maki = {name:'Maki Maki', address: '18-7 Wakamatsucho, Shinjuku, 152-0001', telephone: '030-3092-03939', category: 'japanese' }
-waku-waku = { name: 'Waku Waku', address: '51-3 Kawadacho, Shinjuku, 154-0052', telephone: '030-3092-0339', category: 'japanese' }
-chilovesyou = { name: 'chilovesyou', address: '12-3, Harajuku, Shibuya, 151-0032', telephone: '030-3022-0339', category: 'french' }
-le-chocolat = { name: 'Le chocolat', address: '20-3, Omotesando, Shibuya, 151-0032', telephone: '080-3022-0339', category: 'belgian' }
-pizzerola = { name: 'pizzerola', address: '11-3, Dainkayama, Shibuya, 150-0000', telephone: '030-3088-0339', category: 'italian' }
+maki_maki = {name:'Maki Maki', address: '18-7 Wakamatsucho, Shinjuku, 152-0001', phone_number: '030-3092-03939', category: 'japanese' }
+waku-waku = { name: 'Waku Waku', address: '51-3 Kawadacho, Shinjuku, 154-0052', phone_number: '030-3092-0339', category: 'japanese' }
+chilovesyou = { name: 'chilovesyou', address: '12-3, Harajuku, Shibuya, 151-0032', phone_number: '030-3022-0339', category: 'french' }
+le-chocolat = { name: 'Le chocolat', address: '20-3, Omotesando, Shibuya, 151-0032', phone_number: '080-3022-0339', category: 'belgian' }
+pizzerola = { name: 'pizzerola', address: '11-3, Dainkayama, Shibuya, 150-0000', phone_number: '030-3088-0339', category: 'italian' }
 
 [ maki-maki, waku-waku, chilovesyou, le-chocolat, pizzerola ].each do |attributes|
   restaurant = Restaurant.create!(attributes)
